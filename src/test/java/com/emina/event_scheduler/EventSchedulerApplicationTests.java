61b0ff1edf0f707f1fdb483342b041ebe2090446
eminamarmarac <mac@Macs-MacBook-Pro.local>
2024-09-21 15:01:27 +0200

    Initial commit

---
+++ src/test/java/com/emina/event_scheduler/EventSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emina.event_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
